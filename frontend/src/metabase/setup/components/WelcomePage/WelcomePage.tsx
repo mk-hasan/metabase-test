df476b0fdefd56e8bcb17be57aa146fd9406f7c9
mk-hasan <hasan.alive@gmail.com>
2022-08-18 06:49:00 +0200

    changed welcome page

--- frontend/src/metabase/setup/components/WelcomePage/WelcomePage.tsx
+++ frontend/src/metabase/setup/components/WelcomePage/WelcomePage.tsx
@@ -36,7 +36,7 @@ const WelcomePage = ({
     <PageRoot>
       <PageMain>
         <LogoIcon height={118} />
-        <PageTitle>{t`Welcome to Metabase`}</PageTitle>
+        <PageTitle>{t`Welcome to Springboard`}</PageTitle>
         <PageBody>
           {t`Looks like everything is working.`}{" "}
           {t`Now let’s get to know you, connect to your data, and start finding you some answers!`}
